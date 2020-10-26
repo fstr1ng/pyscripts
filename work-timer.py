a8460bb4c108a6288be737844eaa656bda82e998
Mikhail Fedoseev <mfedoseev@quantumbrains.com>
2020-10-26 12:19:22 +0300

    Modify work-timer.py

--- work-timer.py
+++ work-timer.py
@@ -6,18 +6,22 @@ print("Stopwatcher v 0.1\n")
 
 def cycle():
     print(f"Periods count: {len(periods)}")
-    user_input = input("Press enter to start.")
+    user_input = input("Press enter to start or any to exit. ")
+    if user_input != '':
+        return False
     period_start = datetime.now()
     print(f"Timer started at {period_start}.\n")
 
-    user_input = input("Press enter to pause.")
+    user_input = input("Press enter to pause. ")
     period_end = datetime.now()
     periods.append((period_start,period_end))
     print(f"Paused at {period_end}.\n")
+    return True
 
-for i in range(3):
-    cycle()
+running = True
+
+while running:
+    running = cycle()
 
 for i, period in enumerate(periods):
     print(f"{i}\tstart: {period[0]}\tend: {period[1]}")
-    
